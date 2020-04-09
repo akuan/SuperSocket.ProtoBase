41fd413813684d693cfbab064a56bc47685005f9
Aaron <mouhongkuan@163.com>
2020-04-09 10:55:55 +0800

    添加断线重连

---
+++ src/EE.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using log4net; 
+
+namespace SuperSocket.ProtoBase
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class EE
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="e"></param>
+        /// <param name="log"></param>
+        public static void LogError(string s, Exception e, ILog log)
+        {
+            log.Error($"{s}{e.Message}");
+            log.Error(e.StackTrace);
+            while (e.InnerException != null)
+            {
+                log.Error($"InnerException:{e.InnerException.Message}\r\n{e.InnerException.StackTrace}");
+                e = e.InnerException;
+            }
+        }
+    }
+}
